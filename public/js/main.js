1e3130ec220bf55e0338e2622712943f57ed7b1f
Ethan Barrett <hello@buildingsareheavy.com>
2026-03-20 03:12:51 -0600

    Implemented dynamic rendering for bookmarks and refacotred json structure

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+const main = document.querySelector("main");
+
+async function getBookmarks() {
+  const bookmarksUrl = "/bookmarks";
+  try {
+    const response = await fetch(bookmarksUrl);
+    if (!response.ok) {
+      throw new Error("Response status: " + response.status);
+    }
+    const result = await response.json();
+    console.log(result);
+    main.innerHTML = result
+      .map(
+        (bookmark) => `
+        <article>
+    <h3><a href="${bookmark.url}">${bookmark.title}</a></h3>
+    <div aria-label="meta information" class="meta-info">
+          <p aria-label="tags">${bookmark.tags.map((tag) => `<span> ${tag} </span>`).join("")} </p>
+          <p aria-label="created date">${new Date(
+            bookmark.createdAt,
+          ).toLocaleString("en-GB", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+          })}</p>
+        </div>
+        <button class="modify" aria-label="bookmark options">...</button>
+        </article>
+    `,
+      )
+      .join("");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getBookmarks();
